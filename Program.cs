0e850ff427a6729b90375c7d733a5dec582bca49
move <move@zealand.dk>
2021-09-28 11:22:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using FruitsServer;
+
+namespace FruitServerClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            TestFruit();
+            Console.ReadLine();
+        }
+
+        private static async void TestFruit()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                FruitServerConsumer _consumer = new FruitServerConsumer("http://localhost:58540/", client);
+                var fruits = await _consumer.FruitsAllAsync(null).ConfigureAwait(false);
+
+                foreach (Fruit myFruit in fruits)
+                {
+                    Console.WriteLine(myFruit.TypeOfFruit);
+                }
+            }
+        }
+    }
+}
